7d855ad9fb5771ac62e7ace19750aeadc91630a5
Deepikashini16 <66158887+Deepikashini16@users.noreply.github.com>
2020-08-03 09:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,12 @@ def home():
     with open(filename, 'wb') as f:
         f.write(imgdata)
     names = classify_face("some_image.jpg","")
+    if names == "":
+        return "Unknown"
     return names
 
 
 def get_encoded_faces():
-    """
-    looks through the faces folder and encodes all
-    the faces
-
-    :return: dict of (name, image encoded)
-    """
     encoded = {}
 
     for dirpath, dnames, fnames in os.walk("./faces"):
@@ -39,39 +35,22 @@ def get_encoded_faces():
 
                 encoding = fr.face_encodings(face)[0]
                 encoded[f.split(".")[0]] = encoding
-
-
-
-
     return encoded
 
 
 def unknown_image_encoded(img):
-    """
-    encode a face given the file name
-    """
     face = fr.load_image_file("faces/" + img)
     encoding = fr.face_encodings(face)[0]
-
     return encoding
 
 
 def classify_face(im,String_names):
-    """
-    will find all of the faces in a given image and label
-    them if it knows what they are
-
-    :param im: str of file path
-    :return: list of face names
-    """
+    String_address = ""
     faces = get_encoded_faces()
     faces_encoded = list(faces.values())
     known_face_names = list(faces.keys())
 
     img = cv2.imread(im, 1)
-    #img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-    #img = img[:,:,::-1]
-    last_res = "Unknown"
 
     face_locations = face_recognition.face_locations(img)
     unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
@@ -81,34 +60,13 @@ def classify_face(im,String_names):
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(faces_encoded, face_encoding)
 
-        handles = ""
         for i in range(len(matches)):
             if matches[i]:
-                handles += known_face_names[i]+" "
-
-        # use the known face with the smallest distance to the new face
-        face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-            #print(name)
-
-#         face_names.append(name)
-
-        """for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Draw a box around the face
-            cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)"""
-
-        if handles != "":
-            if last_res == "Unknown":
-                last_res = ""
-            last_res += handles   
-    return last_res
+                file_name="faces/"+known_face_names[i]+".jpg"
+                with open(file_name, "rb") as img_file:
+                    my_string = base64.b64encode(img_file.read())
+                String_address+=str(my_string)+"#"
+    return String_address
 
 if __name__ == '__main__':
     app.run(debug=True)
